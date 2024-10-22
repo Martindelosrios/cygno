c44ef653522ad71a2ab04ba1e9d824b4ea950ff9
Martin de los Rios <martindelosrios13@gmail.com>
2024-10-22 16:04:43 +0200

    new example from cygno

--- codes/ReadingData.py
+++ codes/ReadingData.py
@@ -1,18 +1,38 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 import numpy as np
-from LHCO_reader import LHCO_reader
 import matplotlib.pyplot as plt
+
+
+import ROOT
+
+data = ROOT.TFile.Open("../data/bkg/reco_run84603_3D.root")
+
+nevents = len(data['Events;1']['redpix_ix'].arrays())
+
+
+max_xpix = 0
+max_ypix = 0
+
+for i in np.arange(nevents):
+    if len(data['Events;1']['redpix_ix'].arrays(library='np')['redpix_ix'][i]) > 0:
+        aux = np.max(data['Events;1']['redpix_ix'].arrays(library='np')['redpix_ix'][i])
+        max_xpix = np.max([max_xpix, aux])
+
+    if len(data['Events;1']['redpix_iy'].arrays(library='np')['redpix_iy'][i]) > 0:
+        aux = np.max(data['Events;1']['redpix_iy'].arrays(library='np')['redpix_iy'][i])
+        max_ypix = np.max([max_ypix, aux])
+
+
+xpix = 2295 #max_xpix
+ypix = 2059 #max_ypix
+
+i = 11
+img = np.zeros((xpix, ypix))
+
+xPix_ind = data['Events;1']['redpix_ix'].arrays(library='np')['redpix_ix'][i]
+yPix_ind = data['Events;1']['redpix_iy'].arrays(library='np')['redpix_iy'][i]
+
+img[xPix_ind, yPix_ind] = data['Events;1']['redpix_iz'].arrays(library='np')['redpix_iz'][i]
+
+plt.imshow(img)
+plt.show()
 
